583f65ab61452943b8011b99d7ae82b73f3f223c
fshiper <przemyslaw.krzywodajc@gmail.com>
2020-04-07 11:40:04 +0200

    purgecss init

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,3 +1,17 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: [
+    './src/**/*.js',
+    './public/index.html',
+  ],
+  rejected: true
+})
+
 module.exports = {
-  plugins: [require("tailwindcss"), require("autoprefixer")],
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    ...process.env.NODE_ENV === 'production'
+      ? [purgecss]
+      : []
+  ],
 };
