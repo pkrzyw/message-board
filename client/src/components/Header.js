66cbc532519d4a7a69b8f2bd59c8c2c5fc46ec1c
Przemysław Krzywodajć <przemyslaw.krzywodajc@gmail.com>
2020-04-16 16:49:46 +0200

    changed header style

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,41 +1,46 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { gql } from 'apollo-boost'
-import { useQuery } from '@apollo/react-hooks';
+import React from "react";
+import { Link } from "react-router-dom";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
 
 const BOARDS = gql`
-query {
+  query {
     allBoards {
-        id
-        name
+      id
+      name
     }
-}
-`
+  }
+`;
 
 export default function Header() {
+  const { loading, error, data } = useQuery(BOARDS);
 
-    const { loading, error, data } = useQuery(BOARDS);
-
-    if (error) return <p>Error</p>
-    return (
-        <div className="bg-blue-800 shadow-lg text-gray-200">
-            <div className="pl-3 py-2  text-xl ">
-                Message Board
-        </div>
-            <nav>
-                <Link to="/">Home</Link>
-                <Link to="board/new">New Board</Link>
-                {loading
-                    ? "Fetching"
-                    : data.allBoards.map(board => (
-                        <div key={board.id} className="inline-block ml-4">
-                            <Link to={`board/${board.name}`} state={{ boardId: board.id }}>
-                                {board.name}
-                            </Link>
-                        </div>
-                    ))
-                }
-            </nav>
-        </div >
-    )
+  if (error) return <p>Error</p>;
+  return (
+    <div className="bg-blue-800 shadow-lg text-gray-200">
+      <div className="pl-3 py-2  text-xl ">Message Board</div>
+      <nav>
+        <Link to="/">Home</Link>
+        {" | "}
+        <Link to="board/new">New Board</Link>
+        <ul>
+          {loading
+            ? "Fetching"
+            : data.allBoards.map((board) => (
+                <li
+                  key={board.id}
+                  className="inline-block px-1 ml-4 bg-blue-400 rounded-lg text-xs shadow-sm"
+                >
+                  <Link
+                    to={`board/${board.name}`}
+                    state={{ boardId: board.id }}
+                  >
+                    #{board.name}
+                  </Link>
+                </li>
+              ))}
+        </ul>
+      </nav>
+    </div>
+  );
 }
