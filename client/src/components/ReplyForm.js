cace01060adf222bb1cc5716caaccc887968a1ad
Przemysław Krzywodajć <przemyslaw.krzywodajc@gmail.com>
2020-04-18 19:13:35 +0200

    reply form

---
+++ client/src/components/ReplyForm.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ReplyForm({ threadId }) {
+  return <div>{threadId}</div>;
+}
--- client/src/components/Thread.js
+++ client/src/components/Thread.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Reply from "./Reply";
+import ReplyForm from "./ReplyForm";
 
 export default function Thread({ thread }) {
   const date = new Date(+thread.created_on).toLocaleDateString();
@@ -11,18 +12,17 @@ export default function Thread({ thread }) {
           {thread.text}
         </p>
       </div>
-      <div className="py-1 text-xs">
-        {thread.replies.length > 0 ? (
-          <>
-            Replies
-            <div className="p-1 bg-gray-200 shadow-inner rounded-md w-6/12">
-              {thread.replies.map((reply) => (
-                <Reply key={reply.id} reply={reply} />
-              ))}
-            </div>
-          </>
-        ) : null}
-      </div>
+      {thread.replies.length > 0 ? (
+        <div className="py-1 text-xs">
+          Replies
+          <div className="p-1 bg-gray-200 shadow-inner rounded-md w-6/12">
+            {thread.replies.map((reply) => (
+              <Reply key={reply.id} reply={reply} />
+            ))}
+          </div>
+        </div>
+      ) : null}
+      <ReplyForm threadId={thread.id} />
     </div>
   );
 }
